fa059e1490873e85a10f61c9a8995dd805543bc8
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-12-04 11:22:54 +0530

    fix

--- pages/profile.js
+++ pages/profile.js
@@ -29,7 +29,7 @@ export default function ProfilePage() {
       return;
     }
     fetchUser();
-  }, [userId]);
+  }, [userId,supabase]);
 
   function fetchUser() {
     supabase.from('profiles')
